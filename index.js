5d526ceeb441f27879fed1ed65ab5e4ed4ab61de
komalkalsoom1 <163688131+komalkalsoom1@users.noreply.github.com>
2024-04-24 18:28:22 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+
+console.log("js file connected");
+
+let menuList = document.getElementById('menuList');
+menuList.style.maxHeight = '0px';
+
+function toggleMenu(){
+
+    if(menuList.style.maxHeight == '0px'){
+        menuList.style.maxHeight = '300px'
+    }
+    else{
+        menuList.style.maxHeight = '0px'
+        
+    }
+}
+
